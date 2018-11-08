11cf0c99231a81412d4f4b17feba2c8c13ada501
韩常铭 <han.changming@qq.com>
2018-11-08 10:58:29 +0800

    修复跨域请求401的问题

--- src/main/java/com/rengu/project/board/Configuration/ResourceServerConfig.java
+++ src/main/java/com/rengu/project/board/Configuration/ResourceServerConfig.java
@@ -10,39 +10,14 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-//    @Bean
-//    public TokenStore tokenStore() {
-//        return new JwtTokenStore(accessTokenConverter());
-//    }
-
-//    @Bean
-//    public JwtAccessTokenConverter accessTokenConverter() {
-//        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-//        jwtAccessTokenConverter.setSigningKey("rengu");
-//        return jwtAccessTokenConverter;
-//    }
-
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        corsConfiguration.setAllowCredentials(true);
-//        corsConfiguration.addAllowedOrigin("*");
-//        corsConfiguration.addAllowedHeader("*");
-//        corsConfiguration.addAllowedMethod("*");
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", corsConfiguration);
-//        return source;
-//    }
-
-
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.cors();
         // 放行所有Option请求
         http.authorizeRequests().antMatchers(HttpMethod.OPTIONS).permitAll();
-        // 放行用户注册接口
-        http.authorizeRequests().antMatchers(HttpMethod.POST, "/users/user").permitAll();
+        // 放行看板注册接口
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/boards/register").permitAll();
         // 放行swagger2文档页面
         http.authorizeRequests().antMatchers("/swagger-ui.html", "/webjars/**", "/swagger-resources/**", "/v2/api-docs").permitAll();
         http.authorizeRequests().anyRequest().authenticated();
--- src/main/java/com/rengu/project/board/Configuration/WebSecurityConfig.java
+++ src/main/java/com/rengu/project/board/Configuration/WebSecurityConfig.java
@@ -4,13 +4,17 @@ import com.rengu.project.board.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Order(value = -1)
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -36,5 +40,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 关闭CSRF
+        http.csrf().disable();
+        http.requestMatchers().antMatchers(HttpMethod.OPTIONS, "/**");
     }
 }
